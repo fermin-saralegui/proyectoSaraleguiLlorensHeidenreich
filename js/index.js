942e06da378ad223964be8f8018f919638deed1a
jllorensprovedo <jllorensprovedo@udesa.edu.ar>
2025-11-18 10:02:03 -0300

    agrego los primeros 5 productos a traves de la api

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+let url = 'https://dummyjson.com/products';
+let lista1 = document.querySelector(".cincoprimaleat");
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let productos = data.products; 
+
+    console.log(productos);
+
+    for (let i = 0; i < 5; i++) {
+      lista1.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="">
+          <p>${productos[i].title}</p>
+          <p>${productos[i].category}</p>
+          <p>${productos[i].description}</p>
+          <p>${productos[i].price}</p>
+        </article>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
