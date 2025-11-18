74d06a4b3ba681db879bfb00422a1b76173943a8
manuheidenreich <mheidenreich@udesa.edu.ar>
2025-11-18 10:33:17 -0300

    Agrego los 10 productos mas vendidos

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ fetch(url)
 
     console.log(productos);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i <5; i++) {
       lista1.innerHTML += `
         <article>
           <img src="${productos[i].thumbnail}" alt="">
@@ -38,8 +38,62 @@ fetch(url)
 
     console.log(productos);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 6; i<10; i++) {
       lista2.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="">
+          <p>${productos[i].title}</p>
+          <p>${productos[i].category}</p>
+          <p>${productos[i].description}</p>
+          <p>${productos[i].price}</p>
+        </article>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
+
+  let lista3 = document.querySelector(".cincoprimven");
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let productos = data.products; 
+
+    console.log(productos);
+
+    for (let i = 11; i < 15; i++) {
+      lista3.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="">
+          <p>${productos[i].title}</p>
+          <p>${productos[i].category}</p>
+          <p>${productos[i].description}</p>
+          <p>${productos[i].price}</p>
+        </article>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
+
+  let lista4 = document.querySelector(".cincosegven");
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let productos = data.products; 
+
+    console.log(productos);
+
+    for (let i = 16; i < 20; i++) {
+      lista4.innerHTML += `
         <article>
           <img src="${productos[i].thumbnail}" alt="">
           <p>${productos[i].title}</p>
