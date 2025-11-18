4b7aee38b93bb84e6bd50246dd9e567413ba517b
manuheidenreich <mheidenreich@udesa.edu.ar>
2025-11-18 11:28:11 -0300

    Hago category JS

--- js/category.js
+++ js/category.js
@@ -1,5 +1,32 @@
- let url2 = 'https://dummyjson.com/products/categories';
-  let navmain1 = document.querySelector(".navmainlista");
+let url1 = "https://dummyjson.com/products/category/sports-accesories" 
+let lista1 = document.querySelector(".productosdeportivos")
+
+fetch(url1)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let productos = data.products; 
+
+    console.log(productos);
+
+    for (let i = 0; i <productos.length; i++) {
+      lista1.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="">
+          <p>${productos[i].title}</p>
+          <a class="verdetalle" href="./product.html">Ver detalle</a>
+        </article>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
+
+  
+let url2 = 'https://dummyjson.com/products/categories';
+let navmain1 = document.querySelector(".navmainlista");
 
 fetch(url2)
   .then(function(response) {
@@ -13,7 +40,6 @@ fetch(url2)
     for (let i=0; i<productos.length; i++) {
       navmain1.innerHTML += `
         <li class=" navmainjs">
-
           ${productos[i].name}
         </li>
       `;
