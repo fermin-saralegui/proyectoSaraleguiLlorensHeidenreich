b4e429aa0bcb174e8c3c522dc10dd9c3d4f8d93a
fermin-saralegui <fsaralegui@udesa.edu.ar>
2025-11-18 10:21:47 -0300

    Subo los otros cinco productos aleatorios mediante la api

--- js/index.js
+++ js/index.js
@@ -26,3 +26,30 @@ fetch(url)
   .catch(function(error) {
     console.log("Error: " + error);
   });
+
+  let lista2 = document.querySelector(".cincosegaleat");
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let productos = data.products; 
+
+    console.log(productos);
+
+    for (let i = 0; i < 5; i++) {
+      lista2.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="">
+          <p>${productos[i].title}</p>
+          <p>${productos[i].category}</p>
+          <p>${productos[i].description}</p>
+          <p>${productos[i].price}</p>
+        </article>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
