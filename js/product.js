8c597e30ce874f6fb05183442ec5e3c9513988e0
fermin-saralegui <fsaralegui@udesa.edu.ar>
2025-11-18 11:11:49 -0300

    Subo el producto en productjs y modifico el html para que quede bien

--- js/product.js
+++ js/product.js
@@ -22,3 +22,34 @@ fetch(url2)
   .catch(function(error) {
     console.log("Error: " + error);
   });
+
+let url3 = 'https://dummyjson.com/products';
+let lista3 = document.querySelector(".productodescripcion");
+
+fetch(url3)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let productos = data.products; 
+
+    console.log(productos);
+
+    for (let i = 0; i = 1; i++) {
+      lista3.innerHTML += `
+        <article>
+          <img src="${productos[i].thumbnail}" alt="">
+          <p>${productos[i].title}</p>
+          <p>${productos[i].category}</p>
+          <p>${productos[i].description}</p>
+          <p>${productos[i].price}</p>
+          <p>${productos[i].stock}</p>
+          <p>${productos[i].rating}</p>
+          <p>${productos[i].price}</p>
+        </article>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
