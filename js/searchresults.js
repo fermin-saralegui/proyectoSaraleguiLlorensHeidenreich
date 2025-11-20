7fb4d0e4ab83f55f9f2f434d6c01d312e9a2e466
jllorensprovedo <jllorensprovedo@udesa.edu.ar>
2025-11-20 14:51:30 -0300

    Agrego el SearchResults js para que cuando busques algo aparezca

--- js/searchresults.js
+++ js/searchresults.js
@@ -24,3 +24,49 @@ fetch(('https://dummyjson.com/products/category-list'))
  .catch(function(error) {
    console.log("Error: " + error)
  })
+
+let queryString = location.search
+let urlBu = new URLSearchParams(queryString)
+let searchValores = urlBu.get('search')
+
+let noBuscar = document.querySelector('.nobusqueda')
+let valoresCaja = document.querySelector('.Productosdeportivos1')
+
+noBuscar.innerHTML = "Resultados de tu búsqueda: " + searchValores
+
+fetch('https://dummyjson.com/products/search?q=' + searchValores)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+        let productos = data.products
+        let contenido = ""
+
+        if (productos.length === 0) {
+            noBuscar.innerText = "No hay resultados para: " + searchValores
+            valoresCaja.innerHTML = ""
+        }
+
+        for (let i = 0; i < productos.length; i++) {
+                let p = productos[i]
+    
+                contenido += `
+                    <article class="art">
+                        <img class="imgart" src="${p.thumbnail}" alt="${p.title}">
+                        <h4>${p.title}</h4>
+                        <p>${p.description}</p>
+                        <p>$${p.price}</p>
+                        <button class="vermas">
+                        <a href="./product.html?id=${productos[i].id}" target="_blank">Ver Más</a>
+                        </button>
+                    </article>
+                `
+            }
+        
+
+        valoresCaja.innerHTML = contenido
+    })
+    .catch(function(err){
+        console.log("Error: " + err)
+    })
