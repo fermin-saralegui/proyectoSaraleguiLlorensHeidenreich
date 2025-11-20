4051b4e6de90ff3eb5eb6000cff20bc49c8fe534
jllorensprovedo <jllorensprovedo@udesa.edu.ar>
2025-11-20 15:39:03 -0300

    Agrego Logout.js

---
+++ js/logout.js
@@ -0,0 +1,42 @@
+let logoutBoton = document.querySelector(".logout a");
+
+if (logoutBoton) {
+
+  logoutBoton.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+
+    
+    let loginItem = document.querySelector(".login");
+    let registerItem = document.querySelector(".register");
+
+    if (loginItem) {
+      loginItem.style.display = "block";
+    }
+
+    if (registerItem) {
+      registerItem.style.display = "block";
+    }
+
+    let saludoItem = document.querySelector(".saludo");
+
+    if (saludoItem) {
+      saludoItem.innerHTML = "";
+      saludoItem.style.display = "none";
+    }
+
+    let logoutItem = document.querySelector(".logout");
+
+    if (logoutItem) {
+      logoutItem.style.display = "none";
+    }
+
+    
+    logoutBoton.action = "./index.html";
+    logoutBoton.click();
+  });
+}
